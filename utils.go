0edd96588953bc9ef708dfc193615de6df81b34a
HikariTakahashi <uj242085@cc.seikei.ac.jp>
2025-05-15 21:00:34 +0900

    feat/utils.go/CORS-setting

--- utils.go
+++ utils.go
@@ -7,4 +7,6 @@ import (
 // フロントとバックで別々のドメインよりCORS設定
 func setCORS(w http.ResponseWriter) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 }
