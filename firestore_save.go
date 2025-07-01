d8523e7b149513249a69d80580647cb1a2dc0722
yuttto0024 <yuto0024@outlook.com>
2025-07-01 09:36:31 +0900

    feat:initialize firebase auth client

--- firestore_save.go
+++ firestore_save.go
@@ -9,7 +9,7 @@ import (
 // saveScheduleToFirestore は、指定されたspaceIdのドキュメントとしてデータをFirestoreに保存します。
 // 既存のドキュメントがある場合は、完全に上書きされます。
 func saveScheduleToFirestore(ctx context.Context, spaceId string, data *ScheduleDocument) error {
-	docRef := client.Collection(firestoreCollectionName).Doc(spaceId)
+	docRef := firestoreClient.Collection(firestoreCollectionName).Doc(spaceId)
 	
 	// .Set() メソッドは構造体を渡すと、自動的にFirestoreのドキュメント形式に変換します。
 	_, err := docRef.Set(ctx, data)
