1f46dd31b0f5f8b72b2c3f642125c3baa903b1d5
HikariTakahashi <uj242085@cc.seikei.ac.jp>
2025-08-13 23:42:02 +0900

    hotfix/utils.go/skip-email

--- utils.go
+++ utils.go
@@ -3,15 +3,18 @@ package main
 
 import (
 	"context"
+	"os"
+	"strings"
 )
 
 // Lambda用: CORSヘッダーのマップを返す
 // (main_lambda.go から呼ばれる)
 func getCorsHeaders() map[string]string {
 	return map[string]string{
-		"Access-Control-Allow-Origin":  "*",
-		"Access-Control-Allow-Methods": "POST, GET, OPTIONS, PUT, DELETE",
-		"Access-Control-Allow-Headers": "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization",
+		"Access-Control-Allow-Origin":      "*",
+		"Access-Control-Allow-Methods":     "POST, GET, OPTIONS, PUT, DELETE",
+		"Access-Control-Allow-Headers":     "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization",
+		"Access-Control-Allow-Credentials": "true",
 	}
 }
 
@@ -30,4 +33,36 @@ func setUIDInContext(ctx context.Context, uid string) context.Context {
 func getUIDFromContext(ctx context.Context) (string, bool) {
 	uid, ok := ctx.Value(uidContextKey).(string)
 	return uid, ok
+}
+
+// isLambdaEnvironment は現在の環境がAWS Lambdaかどうかを判定します
+func isLambdaEnvironment() bool {
+	// AWS Lambda環境では以下の環境変数が設定されます
+	lambdaEnvVars := []string{
+		"AWS_LAMBDA_FUNCTION_NAME",
+		"AWS_LAMBDA_FUNCTION_VERSION",
+		"AWS_LAMBDA_LOG_GROUP_NAME",
+		"AWS_LAMBDA_LOG_STREAM_NAME",
+		"AWS_LAMBDA_RUNTIME_API",
+	}
+	
+	for _, envVar := range lambdaEnvVars {
+		if os.Getenv(envVar) != "" {
+			return true
+		}
+	}
+	
+	return false
+}
+
+// shouldSkipEmailVerification はLambda環境でメール送信が失敗した場合にメール認証をスキップするかどうかを判定します
+func shouldSkipEmailVerification() bool {
+	// Lambda環境でのみメール認証スキップを許可
+	if !isLambdaEnvironment() {
+		return false
+	}
+	
+	// 環境変数で明示的にスキップを許可する場合
+	skipEmail := os.Getenv("SKIP_EMAIL_VERIFICATION")
+	return strings.ToLower(skipEmail) == "true"
 }
