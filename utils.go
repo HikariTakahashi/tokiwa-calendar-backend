ab1d26a7de88cd3e13eafda070c2377a1e8252d6
HikariTakahashi <uj242085@cc.seikei.ac.jp>
2025-09-11 15:48:10 +0900

    delete/utils.go/check_lambda

--- utils.go
+++ utils.go
@@ -3,8 +3,6 @@ package main
 
 import (
 	"context"
-	"os"
-	"strings"
 )
 
 // Lambda用: CORSヘッダーのマップを返す
@@ -33,36 +31,4 @@ func setUIDInContext(ctx context.Context, uid string) context.Context {
 func getUIDFromContext(ctx context.Context) (string, bool) {
 	uid, ok := ctx.Value(uidContextKey).(string)
 	return uid, ok
-}
-
-// isLambdaEnvironment は現在の環境がAWS Lambdaかどうかを判定します
-func isLambdaEnvironment() bool {
-	// AWS Lambda環境では以下の環境変数が設定されます
-	lambdaEnvVars := []string{
-		"AWS_LAMBDA_FUNCTION_NAME",
-		"AWS_LAMBDA_FUNCTION_VERSION",
-		"AWS_LAMBDA_LOG_GROUP_NAME",
-		"AWS_LAMBDA_LOG_STREAM_NAME",
-		"AWS_LAMBDA_RUNTIME_API",
-	}
-	
-	for _, envVar := range lambdaEnvVars {
-		if os.Getenv(envVar) != "" {
-			return true
-		}
-	}
-	
-	return false
-}
-
-// shouldSkipEmailVerification はLambda環境でメール送信が失敗した場合にメール認証をスキップするかどうかを判定します
-func shouldSkipEmailVerification() bool {
-	// Lambda環境でのみメール認証スキップを許可
-	if !isLambdaEnvironment() {
-		return false
-	}
-	
-	// 環境変数で明示的にスキップを許可する場合
-	skipEmail := os.Getenv("SKIP_EMAIL_VERIFICATION")
-	return strings.ToLower(skipEmail) == "true"
 }
